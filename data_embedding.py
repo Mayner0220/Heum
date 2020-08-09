e9869aafca9337d8f9a6bb46b41a1fdd483bab18
Mayner <shj030220@gmail.com>
2020-08-09 14:39:14 +0900

    Heum

--- data_embedding.py
+++ data_embedding.py
@@ -1,4 +1,11 @@
 from konlpy.tag import Okt
 from gensim.models.word2vec import Word2Vec
-from data_preprocessing import term_data
+from data_preprocessing import training_data
 import pandas as pd
+
+okt = Okt()
+tokenozed_data = []
+
+for sentence in training_data["term"]:
+    temp_X = okt.morphs(sentence, stem=True)
+    tokenozed_data.append(temp_X)
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,3 +8,6 @@ dataset = pd.read_csv(file_root, encoding="CP949")
 
 # term 열을 제외한 다른 열을 삭제
 term_data = dataset.drop(["category", "tag"], axis=1)
+
+# term_data에서 영어가 포함된 데이터는 제거하기 위해 929143 행 이후의 데이터는 포함하지 않음
+training_data = term_data.iloc[:929142]
