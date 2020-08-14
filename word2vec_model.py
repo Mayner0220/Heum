533865f9ed244f1563a8895578f1aa1e53a7113e
Mayner <shj030220@gmail.com>
2020-08-15 01:06:50 +0900

    Heum

--- word2vec_model.py
+++ word2vec_model.py
@@ -1,7 +1,7 @@
 from gensim.models import Word2Vec
 from data_embedding import tokenozed_data
 
-# sentence parameter에서 에러 발생
+# sentence parameter에서 에러 발생했었음 -> 인자값의 오타로 인하여 발생했었음
 model = Word2Vec(sentences=tokenozed_data, size=100, window=5, min_count=5, workers=4, sg=0)
 
 # 모델 저장
