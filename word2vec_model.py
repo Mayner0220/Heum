e7dff4de7eec5c93021c2d7e149d3550764aeeac
Mayner <shj030220@gmail.com>
2020-08-15 01:00:40 +0900

    Fixed Word2Vec() Error

--- word2vec_model.py
+++ word2vec_model.py
@@ -2,8 +2,7 @@ from gensim.models import Word2Vec
 from data_embedding import tokenozed_data
 
 # sentence parameter에서 에러 발생
-model = Word2Vec(sentence=tokenozed_data, size=100, window=5, min_count=5,
-                 workers=4, sg=0)
+model = Word2Vec(sentences=tokenozed_data, size=100, window=5, min_count=5, workers=4, sg=0)
 
 # 모델 저장
 model.wv.save_word2vec_format("Heum_mark1")
