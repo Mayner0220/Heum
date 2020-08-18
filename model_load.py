5520e2fbb16cba75dc5d7920e4f30cd8e33c0a15
Mayner <shj030220@gmail.com>
2020-08-18 09:05:19 +0900

    Heum

--- model_load.py
+++ model_load.py
@@ -1,5 +1,14 @@
 from gensim.models import KeyedVectors
 
-# 모델 로드
-# Heum 모델은 mark 단위별로 나누어서 저장
-load_model = KeyedVectors.load_word2vec_format("Heum_mark1")
+def load_model():
+    model = input("[Select Heum version]\n>> ")
+
+    # 모델 로드
+    # Heum 모델은 mark 단위별로 나누어서 저장
+    load_model = KeyedVectors.load_word2vec_format(model)
+
+    if(load_model):
+        print("Loaded Successfully!")
+        return load_model
+    else:
+        print("Error: Loading Model")
