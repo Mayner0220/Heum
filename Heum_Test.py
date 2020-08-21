5e0b4c3e62b33d3a8236aac95ccda3d7eea12f33
Mayner <shj030220@gmail.com>
2020-08-22 00:45:00 +0900

    Heum

--- Heum_Test.py
+++ Heum_Test.py
@@ -1,5 +1,5 @@
 from model_load import load_model
+import gensim
 
-heum = load_model()
-
-print(heum.wv.most_similar(""))
+model = gensim.models.Word2Vec.load("./ko.bin")
+print(model.wv.most_similar("한국"))
