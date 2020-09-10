d4f29d009754f9410b35a5e4f0273c8e09704c16
Mayner <shj030220@gmail.com>
2020-09-10 16:56:04 +0900

    [Feat] Add features to allow you to directly adjust the displayed results

--- Heum_Test.py
+++ Heum_Test.py
@@ -9,12 +9,26 @@ Hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')
 # 유사어 확인을 진행할 값을 input()을 통해 입력 받을 수 있도록 변경
 Input_V = input("[Input]\n>> ")
 
+# 보여줄 단어의 갯수를 정할 수 있도록 변경
+while True:
+    Show = int(input("\n[Show]\n>> "))
+
+    if Show > 10:
+        print("Error: input value error")
+        continue
+    elif Show < 0:
+        print("Error: input value error")
+        continue
+    else:
+        break
+
 # return type: list
 Checks = Model.wv.most_similar(Input_V)
 
 # .wv.most_similar과 .most_similar의 차이점은 존재하지 않는 것으로 보임
 # Checks = Model.most_similar("확인")
 
-for check in Checks[:5]:
+print("\n========================[Result]========================")
+for check in Checks[:Show]:
     result = Hangul.sub('', str(check))
     print(result)
